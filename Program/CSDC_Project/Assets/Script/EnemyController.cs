f5dfba9d6b6772318855d2916baba2ac691de4eb
Nissy <lizebag@gmail.com>
2018-10-15 09:40:39 +0900

    囲んでまとめるところまで

---
+++ Program/CSDC_Project/Assets/Script/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    ObjManager obj;
+    public GameObject test;
+    PlayerController player;
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == "Face")
+        {
+
+            this.transform.position = test.transform.position;
+        }
+    }
+    void Start () {
+        player = test.GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
