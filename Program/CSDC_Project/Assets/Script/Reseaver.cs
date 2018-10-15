3030d6ad4c45cf7d81e3a1da754db91035e7fead
DesktopCielo <sorara.sky231@gmail.com>
2018-10-15 23:07:30 +0900

    競合解決

---
+++ Program/CSDC_Project/Assets/Script/Reseaver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reseaver : MonoBehaviour
+{
+
+    public int number = 1000;
+    public void ReturnAccess()
+    {
+        Debug.Log("アクセス成功！！");
+    }
+}
