9fa136f9f487a857fd2d8121b024d8bf6260fa74
NotePC_Cielo <sorara.sky231@gmail.com>
2018-11-19 17:45:11 +0900

    AnimatorControllerの作成

---
+++ CSDC_Project/Assets/Resorces/CommonScene/Model/MotionTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionTest : MonoBehaviour {
+	MotionController MC;
+	Animator animaCSDC;
+
+	// Use this for initialization
+	void Start () {
+		MC = GetComponent<MotionController> ();
+		animaCSDC = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//オフ用
+		MC.AnimaTossOff();
+		MC.AnimaPopOff ();
+
+
+		//投げるモーション
+		if(Input.GetKey(KeyCode.T)){
+			MC.AnimaTossIdle(true);		
+		}else{
+			MC.AnimaToss (true);
+		}
+
+		//弾けるモーション
+		if (Input.GetKey (KeyCode.P)) {
+			MC.AnimaPop (true);
+		}
+
+		//いじめ
+		if (Input.GetKey (KeyCode.I) && !animaCSDC.GetBool("IzimeFlag")) {
+			MC.AnimaIzime(true);
+		}
+		if (Input.GetKey (KeyCode.I) && animaCSDC.GetBool("IzimeFlag")) {
+			MC.AnimaIzime(false);
+		}
+
+		//いじめられ
+		if (Input.GetKey (KeyCode.U) && !animaCSDC.GetBool("IzimerareFlag")) {
+			MC.AnimaIzimerare(true);
+		}
+		if (Input.GetKey (KeyCode.U) && animaCSDC.GetBool("IzimerareFlag")) {
+			MC.AnimaIzimerare(false);
+		}
+
+
+
+	}
+}
