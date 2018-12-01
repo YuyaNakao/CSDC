201b539654526e6505c7189a30e30fdfa5329036
kihamu <yuzi1112m@gmail.com>
2018-12-01 10:01:23 +0900

    ムービーシーンの更新

--- CSDC_Project/Assets/Resorces/MovieScene/Script/MovieScene.cs
+++ CSDC_Project/Assets/Resorces/MovieScene/Script/MovieScene.cs
@@ -2,20 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovieScene : MonoBehaviour {
+public class MovieScene : MonoBehaviour
+{
+
+    [SerializeField]
+    private float MovieTime;        // 動画の再生時間
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        // 時間経過処理
+        MovieTime -= Time.deltaTime;
+
+        // 動画が終了した時、次のシーンに入る
+        if(MovieTime <= 0)
+        {
+            Fade.FadeOut(2);
+        }
+
+        if(Input.GetButtonDown("Player1_Kettei"))
         {
             Fade.FadeOut(2);
 //            SceneNavigator.Instance.Change("Priproment");
         }
-	}
+
+        if (Input.GetButtonDown("Player2_Kettei"))
+        {
+            Fade.FadeOut(2);
+            //            SceneNavigator.Instance.Change("Priproment");
+        }
+
+        if (Input.GetButtonDown("Player3_Kettei"))
+        {
+            Fade.FadeOut(2);
+            //            SceneNavigator.Instance.Change("Priproment");
+        }
+
+        if (Input.GetButtonDown("Player4_Kettei"))
+        {
+            Fade.FadeOut(2);
+            //            SceneNavigator.Instance.Change("Priproment");
+        }
+    }
 }
